f21bfbc95d125d5a04231aa8479b2a372982be5d
jeffHoltStevenson <public1@aascot.com>
2024-05-09 21:41:44 -0700

    Initial Commit

---
+++ Game Timer/Game_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_TimerApp.swift
+//  Game Timer
+//
+//  Created by Jeff Holt on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
